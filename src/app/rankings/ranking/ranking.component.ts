ace9e318a18856517a25ad30e37481863117f2fe
JorgeIs-01 <72407493+JorgeIs-01@users.noreply.github.com>
2021-04-15 18:57:32 +0200

    listar ranking

--- src/app/rankings/ranking/ranking.component.ts
+++ src/app/rankings/ranking/ranking.component.ts
@@ -1,6 +1,8 @@
 import { DataSource } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { Ranking } from 'src/app/models/ranking';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { RankingService } from 'src/app/service/ranking.service';
 
 
@@ -12,7 +14,7 @@ import { RankingService } from 'src/app/service/ranking.service';
 export class RankingComponent implements OnInit {
 
   PerfilRanking : Ranking;
-
+  rankingModel = new Ranking("", "", "");
   constructor(private rankingService: RankingService) {
 
    }
@@ -22,6 +24,40 @@ export class RankingComponent implements OnInit {
     this.PerfilRanking= this.rankingService.getRanking();
     console.log(this.PerfilRanking);
 
+
+}
+
+onFormSubmit(itemForm: any): void {
+
+  this.rankingModel = new Ranking(
+    this.rankingModel[0].NomRanking,
+    this.rankingModel[0].NomProfesor,
+    this.rankingModel[0].Cod);
+
+
+  this.rankingService.ListRanking(this.rankingService).subscribe(
+    (datos: Ranking) => {
+      if (datos!= null) {
+        Swal.fire({
+          position: 'top',
+          icon: 'success',
+          title: 'Profesor modificado.',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.Router.navigate(['/perfil-profesor']);
+
+      }
+      else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'El usuario introducido ya existe',
+        })
+      }
+
+      this.rankingService.setRanking(datos);
+    })
 
 }
 
