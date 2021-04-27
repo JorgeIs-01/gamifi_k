9864cca5cd60a4f1b1f541ad1d814f92a145466a
mcanela30 <marticanela2015@gmail.com>
2021-04-27 19:22:19 +0200

    printar ranking profesor hecho

--- src/app/rankings/ranking/ranking.component.ts
+++ src/app/rankings/ranking/ranking.component.ts
@@ -12,7 +12,7 @@ import { RankingService } from 'src/app/service/ranking.service';
   styleUrls: ['./ranking.component.css']
 })
 export class RankingComponent implements OnInit {
-
+  ListaRanking;
   PerfilRanking : Ranking;
   Router : Router;
   rankingModel = new Ranking("", "", "");
@@ -22,43 +22,19 @@ export class RankingComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.PerfilRanking= this.rankingService.getRanking();
-    console.log(this.PerfilRanking);
+    this.rankingService.ListRanking(this.rankingService).subscribe(
+      (datos: Ranking[]) => {
+
+        this.ListaRanking = datos;
+        console.log(this.ListaRanking);
+
+      }
+    );
 
 
 }
 
 onFormSubmit(itemForm: any): void {
-
-  this.rankingModel = new Ranking(
-    this.rankingModel[0].NomRanking,
-    this.rankingModel[0].NomProfesor,
-    this.rankingModel[0].Cod);
-
-
-  this.rankingService.ListRanking(this.rankingService).subscribe(
-    (datos: Ranking) => {
-      if (datos!= null) {
-        Swal.fire({
-          position: 'top',
-          icon: 'success',
-          title: 'Profesor modificado.',
-          showConfirmButton: false,
-          timer: 1500
-        })
-        this.Router.navigate(['/perfil-profesor']);
-
-      }
-      else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'El usuario introducido ya existe',
-        })
-      }
-
-      this.rankingService.setRanking(datos);
-    })
 
 }
 
